80c8f3010e709ea807c8cce16a117e99c785cdd3
Satyendra Kumar Namdeo <satyendranamdeo78130@gmail.com>
2025-03-01 12:56:38 +0530

    Attachment Files Uploaded

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.btn-primary').addEventListener('click', function() {
+        alert('Thank you for joining the Solo Leveling Fitness Challenge!');
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    let video = document.getElementById('bg-video');
+    video.muted = true; // Enable audio
+    video.play();
+});
